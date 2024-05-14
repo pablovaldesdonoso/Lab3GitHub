e46d621f5cf04bb5fc437795abbd307bdcf5bdf7
Pablo <pablovaldes@gmail.com>
2024-05-14 18:26:00 -0400

    Update getting_ready_op2.R
    test for pushing changes

--- getting_ready_op2.R
+++ getting_ready_op2.R
@@ -8,3 +8,7 @@ new_data <- read.csv("/Volumes/PVD2/1_Montreal/1_UdeM/3_Courses/Animal_Data_Scie
 summary(new_data) # Summarize each variable. 
 
 new_data %>% ggplot(aes(x = x0)) + geom_histogram()
+
+
+# let's cerate a boxplot for the same variable "x0"
+new_data %>% ggplot(aes(x = x0)) + geom_box()
